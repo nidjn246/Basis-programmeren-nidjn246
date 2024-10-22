5399d24401ffd4647476acdf6daf7ed8a1a9c0e7
nidjn246 <milanveldhuizen09@gmail.com>
2024-10-22 15:07:44 +0200

    almost done

--- Week 7&8 virtueel huisdier/Week 7&8/sketch.js
+++ Week 7&8 virtueel huisdier/Week 7&8/sketch.js
@@ -1,4 +1,7 @@
-let size = 10
+let startgame = false
+let nameanimal
+let bground
+let size = 15
 let hunger = 100
 let energy = 100
 let health = 100
@@ -6,105 +9,197 @@ let happiness = 100
 let larryState
 let resetbar = 0
 let resetTimer = 0
-let stats = []
+let stats = 0
+let buttonTimer= 0
+let fur = 0
+let menuOpen = false
 
+// all of the states larry can output
 let states = {
   idle: 0,
   low_health: 1,
   lower_health: 2,
-  death: 3
+  death: 3,
+  littlecry: 4,
+  hardcry: 5,
+  littletired: 6,
+  tired: 7,
+  hungry: 8,
+  starving: 9,
 }
 
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(700, 450);
   createButtons()
+  // changeBackground()
+  //load all the data from the local storage to the game
   loadData()
 }
+
+//load the background image
+function preload(){
+  bground = loadImage ("/../Images/background.jpg")
+}
+
 function draw() {
-  reset()
-  background(220)
+  console.log (fur)
+  background(bground)
   saveData()
+    //make the reset button work
+    reset()
+
   happiness = constrain(happiness, 0, 100)
-  hunger = constrain(hunger, 100, 200)
+  hunger = constrain(hunger, 0, 100)
   health = constrain(health, 0, 100)
   energy = constrain(energy, 0, 100)
   drawBars()
-  textSize ( 20)
+  textSize ( 30)
   fill (0)
   textFont ("comic sans ms")
-  text ("Larry", 240, 90)
+  text (nameanimal, 310, 140)
   textSize (15)
-  text ("Hold Esc to reset Larry", 10, 370)
+  text ("Hold Esc to reset Larry", 10, 30)
   checkState()
   drawAnimal()
 
-  if (hunger < 200){
-    hunger = hunger + deltaTime / 2000
+  if (menuOpen == true){
+    fill (255)
+    rect (301, 10, 350, 80)
+    blackColor.show()
+    greenColor.show()
+    redColor.show()
+  }
+  else {
+    blackColor.hide()
+    greenColor.hide()
+    redColor.hide()
+  }
+
+  if (hunger > 0){
+    hunger = hunger - deltaTime / 2000
   }
   
   if (energy > 0){
     energy = energy - deltaTime / 2500
   }
   
-  if (hunger > 199){
+  if (hunger < 1){
     health = health - deltaTime / 4000
   }
   
   if (happiness > 0){
     happiness = happiness - deltaTime / 5000
   }
+
+  //roept een functie aan na x tijd
+  //setTimeout(resetKnoppen, 300);
 }
 
-function saveData (){
+
+function resetKnoppen()
+{
+
+}
+
+
+  function saveData (){
   stats = []
-  stats.push (hunger, energy, health, happiness)
+  stats.push (hunger, energy, health, happiness, fur)
   storeItem('hungerstat', stats[0])
   storeItem('energystat', stats[1])
   storeItem('healthstat', stats[2])
   storeItem('happinessstat', stats[3])
+  storeItem('colorfur', stats[4])
 }
 
+// function changeBackground (){
+//   rightButton = createImage ("/../Images/rightarrow.png")
+//   rightButton.Size (70, 70)
+//   rightButton.position (100, 100)
+// }
+
 function loadData(){
-  hunger = getItem("hungerstat")
-  energy = getItem("energystat")
-  health = getItem("healthstat")
-  happiness = getItem("happinessstat")
+  hunger = getItem("hungerstat") || 100
+  energy = getItem("energystat") || 100
+  health = getItem("healthstat") || 100
+  happiness = getItem("happinessstat") || 100
+  fur = getItem("colorfur") || 0
 }
 
 function createButtons(){
-  foodButton = createButton("Feed")
-  foodButton.position(100, 100)
+  foodButton = createImg("/../Images/burger.png")
+  foodButton.size (70, 70)
+  foodButton.position(100, 370)
   foodButton.mouseClicked(giveFood)
 
-  energyButton = createButton("Play with Larry")
-  energyButton.position(100, 140)
-  energyButton.mouseClicked(play)
+  playButton = createImg("/../Images/play.png")
+  playButton.position(250, 370)
+  playButton.size (80, 70)
+  playButton.mouseClicked(play)
+  
+  sleepButton = createImg("/../Images/bed.png")
+  sleepButton.position(400, 380)
+  sleepButton.size (80, 50)
+  sleepButton.mouseClicked(sleep)
 
-  HealthButton = createButton("Clean")
-  HealthButton.position(100, 220)
+  HealthButton = createImg("/../Images/soap.png")
+  HealthButton.size (80, 80)
+  HealthButton.position(550, 370)
   HealthButton.mouseClicked(heal)
 
-  PlayButton = createButton("Sleep")
-  PlayButton.position(100, 180)
-  PlayButton.mouseClicked(sleep)
+  menu = createImg ("/../Images/hamburgermenu.png")
+  menu.position (650, 10)
+  menu.size (40, 40)
+  menu.mouseClicked (menuBar)
+
+  blackColor = createImg ("/../Images/black.jpeg")
+  blackColor.size (70, 70)
+  blackColor.position (320, 15)
+  blackColor.mouseClicked(black1)
+
+  greenColor = createImg ("/../Images/green.png")
+  greenColor.size (70, 70)
+  greenColor.position (440, 15)
+  greenColor.mouseClicked(green1)
+
+  redColor = createImg ("/../Images/red.jpg")
+  redColor.size (70, 70)
+  redColor.position (560, 15)
+  redColor.mouseClicked(red1)
+}
+
+function black1 (){
+  fur = [0]
+}
+
+function green1 (){
+  fur = [4, 130, 4]
+}
+
+function red1 (){
+  fur = "red"
+}
+
+function menuBar(){
+ menuOpen = !menuOpen
 }
 
 function giveFood(){
-hunger -= 30;
-if (hunger < 101){
-  health -= 10
-}
-
-energy += 10;
-
+  hunger += 30;
+  if (hunger > 100){
+    health -= 10
+  }
+  
+  energy += 10;
+  happiness += 5;
 }
 
 function sleep (){
 energy += 80;
 happiness -= 10
 health += 5
-hunger += 20;
+hunger -= 20;
 
 }
 
@@ -133,20 +228,28 @@ function checkState(){
     larryState = states.death
   }
 
+  else if (hunger > 25 && hunger < 50){
+    larryState = states.hungry
+  }
+
+  else if (hunger < 25){
+    larryState = states.starving
+  }
+
   else if (happiness < 50 && happiness > 25){
-    larryState = 4
+    larryState = states.littlecry
   }
 
   else if (happiness < 25){
-    larryState = 5
+    larryState = states.hardcry
   }
 
   else if (energy < 50 && energy > 25){
-    larryState = 6
+    larryState = states.littletired
   }
 
   else if (energy < 25){
-    larryState = 7
+    larryState = states.tired
   }
 
   else {
@@ -158,45 +261,47 @@ function drawBars(){
   textSize(13)
   //hunger
   strokeWeight (1)
+  fill ("green")
+  rect (450, 100, 10, 100)
   fill (255)
-  rect (350, 100, 10, 100)
-  fill ("Green")
-  rect (350, 200, 10, 100 - hunger)
+  rect (450, 100, 10, 100 - hunger)
   fill (0)
-  text ("Hunger", 340, 210)
+  text ("Food", 440, 210)
 
   //energy
   strokeWeight (1)
   fill ("yellow")
-  rect (410, 100, 10, 100)
+  rect (510, 100, 10, 100)
   fill (255)
-  rect (410, 100, 10, 100 - energy)
+  rect (510, 100, 10, 100 - energy)
   fill(0)
-  text ("Energy", 395, 210)
+  text ("Energy", 495, 210)
 
   //Health
   strokeWeight (1)
   fill ("red")
-  rect (470, 100, 10, 100)
+  rect (570, 100, 10, 100)
   fill (255)
-  rect (470, 100, 10, 100 - health)
+  rect (570, 100, 10, 100 - health)
   fill(0)
-  text ("Health", 455, 210)
+  text ("Health", 555, 210)
 
   //Happiness
   strokeWeight (1)
   fill ("orange")
-  rect (530, 100, 10, 100)
+  rect (630, 100, 10, 100)
   fill (255)
-  rect (530, 100, 10, 100 - happiness)
+  rect (630, 100, 10, 100 - happiness)
   fill(0)
-  text ("happiness", 510, 210)
+  text ("happiness", 610, 210)
 
   //resetbar
+  if (keyIsDown (ESCAPE)){
   fill (255)
-  rect (10, 380, 160, 15)
+  rect (10, 40, 160, 15)
   fill ("red")
-  rect (10, 380, resetbar, 15)
+  rect (10, 40, resetbar, 15)
+  }
 }
 
 
@@ -207,8 +312,8 @@ function drawAnimal() {
     [
       [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
       [0, 0, 2, 1, 1, 3, 3, 3, 1, 1, 2, 0],
       [0, 0, 0, 2, 1, 1, 3, 1, 1, 2, 0, 0],
@@ -221,12 +326,12 @@ function drawAnimal() {
     [
       [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
-      [0, 0, 2, 1, 4, 1, 2, 1, 4, 1, 2, 0],
-      [0, 0, 2, 1, 4, 3, 3, 3, 4, 1, 2, 0],
-      [0, 0, 0, 2, 1, 1, 3, 1, 1, 2, 0, 0],
-      [0, 0, 2, 2, 1, 1, 1, 1, 1, 2, 2, 0],
+      [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
+      [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
+      [0, 0, 2, 1, 1, 3, 3, 3, 1, 1, 2, 0],
+      [0, 0, 0, 2, 1, 6, 3, 6, 1, 2, 0, 0],
+      [0, 0, 2, 2, 1, 6, 1, 1, 1, 2, 2, 0],
       [0, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2],
       [0, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2],
       [0, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2],
@@ -235,8 +340,8 @@ function drawAnimal() {
     [
       [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
+      [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
       [0, 0, 2, 1, 4, 1, 2, 1, 4, 1, 2, 0],
       [0, 0, 2, 1, 4, 3, 3, 3, 4, 1, 2, 0],
       [0, 0, 0, 2, 1, 1, 3, 1, 1, 2, 0, 0],
@@ -263,8 +368,8 @@ function drawAnimal() {
     [
       [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
       [0, 0, 2, 1, 5, 1, 2, 1, 5, 1, 2, 0],
       [0, 0, 2, 1, 1, 3, 3, 3, 1, 1, 2, 0],
       [0, 0, 0, 2, 1, 1, 3, 1, 1, 2, 0, 0],
@@ -277,8 +382,8 @@ function drawAnimal() {
     [
       [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
       [0, 0, 2, 1, 5, 1, 2, 1, 5, 1, 2, 0],
       [0, 0, 2, 1, 5, 3, 3, 3, 5, 1, 2, 0],
       [0, 0, 0, 2, 5, 1, 3, 1, 5, 2, 0, 0],
@@ -292,7 +397,7 @@ function drawAnimal() {
       [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
-      [0, 0, 2, 1, 2, 1, 2, 1, 2, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
       [0, 0, 2, 1, 1, 3, 3, 3, 1, 1, 2, 0],
       [0, 0, 0, 2, 1, 1, 3, 1, 1, 2, 0, 0],
@@ -306,7 +411,7 @@ function drawAnimal() {
       [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
-      [0, 0, 2, 2, 2, 1, 2, 1, 2, 2, 2, 0],
+      [0, 0, 2, 7, 7, 1, 2, 1, 7, 7, 2, 0],
       [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
       [0, 0, 2, 1, 1, 3, 3, 3, 1, 1, 2, 0],
       [0, 0, 0, 2, 1, 1, 3, 1, 1, 2, 0, 0],
@@ -315,19 +420,49 @@ function drawAnimal() {
       [0, 2, 2, 2, 1, 1, 1, 1, 1, 2, 2, 2],
       [0, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2],
       [0, 0, 0, 3, 3, 3, 0, 3, 3, 3, 0, 0],
+    ],
+    [
+      [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
+      [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
+      [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
+      [0, 0, 2, 1, 1, 3, 3, 3, 1, 1, 2, 0],
+      [0, 0, 0, 2, 2, 1, 3, 1, 2, 2, 0, 0],
+      [0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0],
+      [0, 0, 0, 0, 2, 2, 1, 2, 2, 0, 0, 0],
+      [0, 0, 0, 0, 0, 2, 1, 2, 0, 0, 0, 0],
+      [0, 0, 0, 1, 2, 2, 2, 2, 2, 0, 0, 0],
+      [0, 0, 0, 3, 3, 3, 0, 3, 3, 3, 0, 0],
+    ],
+    [
+      [0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0],
+      [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
+      [0, 0, 2, 1, 7, 1, 2, 1, 7, 1, 2, 0],
+      [0, 0, 2, 1, 1, 1, 2, 1, 1, 1, 2, 0],
+      [0, 0, 2, 1, 1, 3, 3, 3, 1, 1, 2, 0],
+      [0, 0, 0, 2, 2, 1, 3, 1, 2, 2, 0, 0],
+      [0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0],
+      [0, 0, 0, 0, 2, 0, 0, 0, 2, 0, 0, 0],
+      [0, 0, 0, 3, 3, 3, 0, 3, 3, 3, 0, 0],
     ]
   ]
 
   let colorpallet = [
     color(0, 0, 0, 0),
     color(255),
-    color(0),
+    color(fur),
     color(250, 128, 11),
     color(255, 0, 0),
-    color(0, 100, 255)
+    color(0, 100, 255),
+    color(0, 180, 0),
+    color(0)
   ];
 
-  drawImage(200, 100, animal[larryState], colorpallet);
+  drawImage(250, 160, animal[larryState], colorpallet);
 }
 
 function drawImage(startX, startY, image, palette) {
@@ -342,7 +477,6 @@ function drawImage(startX, startY, image, palette) {
 
 function reset (){
   if (keyIsDown(ESCAPE)){
-    console.log (resetTimer)
     resetTimer = resetTimer + deltaTime / 1000
     resetbar++
   }
@@ -357,5 +491,16 @@ function reset (){
     energy = 100
     health = 100
     happiness = 100
+    fur = 0
   }
+}
+
+function keyPressed(){
+  if (keyIsPressed == true){
+    if (keyCode == 32){
+    fur = [random(0, 255), random(0, 255), random(0, 255)]
+  }
+}
+
+
 }
