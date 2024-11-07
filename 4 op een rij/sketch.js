56fd6996aee135f9d97e6e397b11565a0af43b0f
nidjn246 <milanveldhuizen09@gmail.com>
2024-11-07 13:14:55 +0100

    alles af behalve checken of je wint

--- 4 op een rij/sketch.js
+++ 4 op een rij/sketch.js
@@ -1,12 +1,24 @@
-let playerturn = 0
-
+let playerturn = 1
 function setup() {
   createCanvas(700, 500);
 }
 
 function draw() {
-  background(220);
+ background(255);
+  strokeWeight (0)
+  fill (0, 103, 208)
+  rect (145, 65, 400, 350)
   drawGrid()
+  fill (0)
+  textSize (50)
+  text ("Player: " + playerturn, 250, 50)
+  textSize (20)
+  text ("Player 1:", 560, 200)
+  text ("Player 2:", 560, 270)
+  fill (255, 0, 0)
+  circle (670, 195, 30)
+  fill (255, 255, 0)
+  circle (670, 265, 30)
 }
 
 let grid =
@@ -20,12 +32,12 @@ let grid =
     [0, 0, 0, 0, 0, 0]
   ]
 function drawGrid() {
-
+  strokeWeight (1)
   let colorPallet =
     [
       color(255), //white
+      color(255, 255, 0),//yellow
       color(255, 0, 0), //red
-      color(255, 255, 0)//yellow
     ]
 
   let rowHeight = 55;
@@ -33,27 +45,30 @@ function drawGrid() {
   for (let x = 0; x < grid.length; x++) {
     for (let y = 0; y < grid[x].length; y++) {
       let maxHeight = (grid[x].length - 1) * rowHeight;
-
       fill(colorPallet[grid[x][y]])
-      circle(x * 55 + 160, (maxHeight - (y * rowHeight)) + 100, 50)
+      circle(x * 55 + 180, (maxHeight - (y * rowHeight)) + 100, 50)
     }
   }
 }
 
 let colomnClicked
-let colomnHeight = -1
+let colomnHeight
 function mouseClicked() {
-  playerturn++
-  if (playerturn > 2) {
-    playerturn = 1
-  }
-  if (colomnHeight < 5){
+  if (colomnHeight < 5) {
     colomnHeight++
   }
-  if (mouseX > 133 && mouseX < 545) {
-    colomnClicked = floor((mouseX - 133) / 55 + 1)
-    grid[colomnClicked - 1][colomnHeight] = playerturn
+  if (mouseX > 133 && mouseX < 545 && mouseY > 73 && mouseY < 400) {
+    colomnClicked = floor((mouseX - 155) / 55)
   }
-
   
+  for (colomnHeight = 0; colomnHeight < grid[colomnClicked].length; colomnHeight++) {
+    if (grid[colomnClicked][colomnHeight] == 0) {
+      grid[colomnClicked][colomnHeight] = playerturn;
+      playerturn++
+      if (playerturn > 2) {
+        playerturn = 1
+      }
+        break;
+    }
+  }
 }
